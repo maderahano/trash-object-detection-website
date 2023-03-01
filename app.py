2bafe22bcfbd3f45850f373f8bf7112f8344cc12
maderahano <mdrahano12@gmail.com>
2023-03-01 15:32:13 +0700

    Add Image Detection Feature

--- app.py
+++ app.py
@@ -1,54 +1,93 @@
 from re import DEBUG, sub
-from flask import Flask, render_template, request, redirect, send_file, url_for
+from flask import Flask, render_template, request, redirect, send_file, url_for, flash, jsonify
 from werkzeug.utils import secure_filename, send_from_directory
 import os
 import subprocess
 
 app = Flask(__name__)
 
-uploads_dir = os.path.join(app.instance_path, 'uploads')
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-os.makedirs(uploads_dir, exist_ok=True)
+app.secret_key = "maderahano"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+upload_images = os.path.join(UPLOAD_FOLDER, 'images')
+upload_videos = os.path.join(UPLOAD_FOLDER, 'videos')
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# upload_images = os.path.join(app.instance_path, 'uploads')
+# os.makedirs(upload_images, exist_ok=True)
 
 @app.route("/")
-def hello_world():
+def main():
     return render_template('index.html')
 
 
-@app.route("/detect", methods=['POST'])
-def detect():
+@app.route("/detect-image", methods=['POST'])
+def detect_image():
     if not request.method == "POST":
         return
-    video = request.files['video']
-    video.save(os.path.join(uploads_dir, secure_filename(video.filename)))
-    print(video)
-    # subprocess.run(['cd', 'yolov4'])
-    subprocess.run(['./darknet', 'detector', 'test', 'data/obj.data', 'cfg/trash.cfg', 'backup/trash/training/trash_best.weights', os.path.join(uploads_dir, secure_filename(video.filename)), '-thresh 0.3'], cwd='yolov4')
+    
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    
+    files = request.files.getlist('file')
+    fileNames = []
+    errors = {}
+    success = False
+
+    for file in files: 
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(upload_images, filename))
+            fileNames.append(filename)
+            success = True
+            subprocess.run(['./darknet', 'detector', 'test', 'data/obj.data', 'cfg/trash.cfg', 'backup/trash/training/trash_best.weights', os.path.join("../static/uploads/images/", filename), '-thresh 0.3', '-dont_show'], cwd='yolov4')
+            subprocess.run(['cp', 'predictions.jpg', '../static/downloads/images/'], cwd='yolov4')
+            subprocess.run(['cp', 'predictions.jpg', filename], cwd='static/downloads/images')
+            subprocess.run(['rm', 'predictions.jpg'], cwd='static/downloads/images')
+        else:
+            errors[file.filename] = 'file type is not allowerd'
+    
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 206
+        return resp
+    if success:
+        resp = jsonify({'message' : "Files successfully uploaded"})
+        resp.status_code = 201
+        return render_template('index.html', filenames=fileNames) 
+    else:
+        resp = jsonify(errors)
+        resp.status_code = 400
+        return resp
+    
+@app.route("/detect-image/<filename>")
+def display_images(filename):
+    print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/images/' + filename), code=301)
+
+# @app.route("/opencam", methods=['GET'])
+# def opencam():
+#     print("here")
+#     subprocess.run(['python3', 'detect.py', '--source', '0'])
+#     return "done"
     
 
-    # return os.path.join(uploads_dir, secure_filename(video.filename))
-    obj = secure_filename(video.filename)
-    return obj
-
-@app.route("/opencam", methods=['GET'])
-def opencam():
-    print("here")
-    subprocess.run(['python3', 'detect.py', '--source', '0'])
-    return "done"
-    
-
-@app.route('/return-files', methods=['GET'])
-def return_file():
-    obj = request.args.get('obj')
-    loc = os.path.join("runs/detect", obj)
-    print(loc)
-    try:
-        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
-        # return send_from_directory(loc, obj)
-    except Exception as e:
-        return str(e)
-
-# @app.route('/display/<filename>')
-# def display_video(filename):
-# 	#print('display_video filename: ' + filename)
-# 	return redirect(url_for('static/video_1.mp4', code=200))
+# @app.route('/return-files', methods=['GET'])
+# def return_file():
+#     obj = request.args.get('obj')
+#     loc = os.path.join("runs/detect", obj)
+#     print(loc)
+#     try:
+#         return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
+#         # return send_from_directory(loc, obj)
+#     except Exception as e:
+#         return str(e)
--- static/js/index.js
+++ static/js/index.js
@@ -1,68 +1,333 @@
-window.onload = () => {
-  $("#sendbutton").click(() => {
-    imagebox = $("#imagebox");
-    link = $("#link");
-    input = $("#imageinput")[0];
-    if (input.files && input.files[0]) {
-      let formData = new FormData();
-      formData.append("video", input.files[0]);
-      $.ajax({
-        url: "/detect", // fix this to your liking
-        type: "POST",
-        data: formData,
-        cache: false,
-        processData: false,
-        contentType: false,
-        error: function (data) {
-          console.log("upload error", data);
-          console.log(data.getAllResponseHeaders());
-        },
-        success: function (data) {
-          console.log(data);
-          // bytestring = data["status"];
-          // image = bytestring.split("'")[1];
-          $("#link").css("visibility", "visible");
-          $("#download").attr("href", "static/" + data);
-          console.log(data);
-        },
-      });
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', ()=> {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', ()=> {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+      skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
     }
-  });
-  $("#opencam").click(() => {
-    console.log("evoked openCam");
-    $.ajax({
-      url: "/opencam",
-      type: "GET",
-      error: function (data) {
-        console.log("upload error", data);
-      },
-      success: function (data) {
-        console.log(data);
-      },
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+/*==================== IMAGE DETECTION MODAL ====================*/
+const imageDetectionModalViews = document.querySelectorAll('.image-detection__modal'),
+      imageDetectionModalBtns = document.querySelectorAll('.image-detection__content')
+    
+let imageDetectionModal = function(imageDetectionModalClick){
+    imageDetectionModalViews[imageDetectionModalClick].classList.add('active-modal-image-detection')
+}
+
+imageDetectionModalBtns.forEach((imageDetectionModalBtn, i) => {
+    imageDetectionModalBtn.addEventListener('click', () => {
+        imageDetectionModal(i)
+    })
+})
+
+/* Modal Upload */
+const uploadCustom = document.querySelector('#upload_custom');
+
+let file;
+
+// uploadCustom.addEventListener('dragover', (event) => {
+//     event.preventDefault();
+//     console.log("Drag Over");
+// });
+
+// uploadCustom.addEventListener('dragleave', () => {
+//     console.log("Drag Leave");
+// });
+
+$(document).ready(function (e) {
+    $('#upload_custom').on('click', function () {
+        $('#upload_custom').change(function () {
+            var formData = new FormData();
+            var ins = document.getElementById('upload_custom').files.length;
+    
+            for (var i = 0; i < ins; i++) {
+                formData.append("file", document.getElementById('upload_custom').files[i]);
+            }
+    
+            $.ajax({
+                url: '/detect-image',
+                dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: formData,
+                type: 'post',
+                success: function (response) {
+                    console.log("upload success", response);
+                    $('#msg').html('');
+                    $.each(response, function(key, data) {
+                        if (key !== 'message') {
+                            $('#msg').append(key + ' -> ' + data + '<br/>');
+                        } else {
+                            $('#msg').append(data + '<br/>');
+                        }
+                    })
+                },
+                error: function (response) {
+                    console.log("upload error", response);
+                    console.log(response.getAllResponseHeaders());
+                    $('#msg').html(response.message);
+                }
+            });
+
+            imageDetectionModalViews.forEach((imageDetectionModalView) => {
+                imageDetectionModalView.classList.remove('active-modal-image-detection')
+            })
+        });
     });
-  });
-};
 
-function readUrl(input) {
-  imagebox = $("#imagebox");
-  console.log(imagebox);
-  console.log("evoked readUrl");
-  if (input.files && input.files[0]) {
-    let reader = new FileReader();
-    reader.onload = function (e) {
-      console.log(e.target);
+    $('#upload_custom').on('drop', function () {
+        $('#upload_custom').change(function () {
+            var formData = new FormData();
+            var ins = document.getElementById('upload_custom').files.length;
 
-      imagebox.attr("src", e.target.result);
-      //   imagebox.height(500);
-      //   imagebox.width(800);
-    };
-    reader.readAsDataURL(input.files[0]);
-  }
+            for (var i = 0; i < ins; i++) {
+                formData.append("file", document.getElementById('upload_custom').files[i]);
+            }
+
+            $.ajax({
+                url: 'detect-image',
+                dataType: 'json',
+                cache: false,
+                contentType: false,
+                processData: false,
+                data: formData,
+                type: 'post',
+                success: function (response) {
+                    console.log("upload success", response);
+                    $('#msg').html('');
+                    $.each(response, function(key, data) {
+                        if (key !== 'message') {
+                            $('#msg').append(key + ' -> ' + data + '<br/>');
+                        } else {
+                            $('#msg').append(data + '<br/>');
+                        }
+                    });
+                },
+                error: function (response) {
+                    console.log("upload error", response);
+                    console.log(response.getAllResponseHeaders());
+                    $('#msg').html(response.message);
+                }  
+            });
+
+            imageDetectionModalViews.forEach((imageDetectionModalView) => {
+                imageDetectionModalView.classList.remove('active-modal-image-detection')
+            })
+        });
+    });
+});
+
+// uploadCustom.addEventListener('click', () => {
+//     uploadCustom.onchange = ({target}) => {
+//         let file = target.files[0];
+
+//         let fileType = file.type;
+//         let validationExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
+    
+//         if(validationExtensions.includes(fileType)) {
+//             let fileReader = new FileReader();
+    
+//             fileReader.onload = () => {
+//                 let fileURL = fileReader.result;
+//                 console.log(fileURL);
+//             };
+//             fileReader.readAsDataURL(file);
+//         } else {
+//             alert('This file is not an image!');
+//         }
+    
+//         imageDetectionModalViews.forEach((imageDetectionModalView) => {
+//             imageDetectionModalView.classList.remove('active-modal-image-detection')
+//         })
+//     }
+// });
+
+// uploadCustom.addEventListener('drop', (event) => {
+//     event.preventDefault();
+
+//     file = event.dataTransfer.files[0];
+
+//     let fileType = file.type;
+//     let validationExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
+
+//     if(validationExtensions.includes(fileType)) {
+//         let fileReader = new FileReader();
+
+//         fileReader.onload = () => {
+//             let fileURL = fileReader.result;
+//             console.log(fileURL);
+//         };
+//         fileReader.readAsDataURL(file);
+//     } else {
+//         alert('This file is not an image!');
+//     }
+
+//     imageDetectionModalViews.forEach((imageDetectionModalView) => {
+//         imageDetectionModalView.classList.remove('active-modal-image-detection')
+//     })
+// });
+
+/*==================== PORTFOLIO SWIPER ====================*/
+let swiperPortfolio = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+/*==================== TESTIMONIAL SWIPER ====================*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
+
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+
+    //When the scroll is highet than 80 viewport height, add the show-scroll class to the tag scroll-up tag
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously these a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know is we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
 
-function openCam(e) {
-  console.log("evoked openCam");
-  e.preventDefault();
-  console.log("evoked openCam");
-  console.log(e);
-}
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    // We save the theme and the current icon that user close
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-item', getCurrentIcon())
+})
